8cf60c88cc9c0124c0d6c100b28e730b16f7ac15
Peter Szabo <peter_szabo90@hotmail.com>
2024-06-13 21:03:08 +0200

    Dates in correct position

--- datarequest.js
+++ datarequest.js
@@ -1,8 +1,5 @@
 let currentDatas;
-let mousePosition = {
-  x: 0,
-  y: 0,
-};
+let mousePosition = {x: 0, y: 0,};
 let canvasDragging = {
     initialX: 0,
     initialY: 0,
@@ -11,43 +8,39 @@ let canvasDragging = {
     zoomModifierX: 0,
     zoomModifierY: 0
 };
-let viewRange = {
-    startIndex: 0, 
-    endIndex: 0
-};
+let viewRange = { startIndex: 0, endIndex: 0};
 let chartMinValue;
 let chartMaxValue;
+
+const submitBtn = document.getElementById('submit-btn');
+const testText = document.getElementById('test');
 const canvas = document.getElementById('stock-chart');
 const dateBar = document.getElementById('date-bar');
 const priceBar = document.getElementById('price-bar');
 const form = document.getElementById('data-request-form');
-let canvasHeight =
-  document.documentElement.clientHeight -
-  form.offsetHeight -
-  dateBar.offsetHeight;
-let canvasWidth = canvas.offsetWidth;
-console.log('document.documentElement.clientHeight ' + document.documentElement.clientHeight);
-  console.log('form.offsetHeight ' + form.offsetHeight);
-console.log('dateBar.offsetHeight ' + dateBar.offsetHeight);
-  console.log('canvasHeight ' + canvasHeight);
-dateBar.width = canvasWidth;
-priceBar.height = canvasHeight;
 
-const submitBtn = document.getElementById('submit-btn');
-const testText = document.getElementById('test');
+canvas.height =
+  document.documentElement.clientHeight -
+    form.offsetHeight -
+dateBar.offsetHeight;
+canvas.width = canvas.offsetWidth;
+dateBar.width = canvas.width;
+priceBar.height = canvas.height;
+
 
 submitBtn.addEventListener('click', requestData);
 canvas.addEventListener('wheel', zoom);
 canvas.addEventListener('mousedown', dragCanvas);
 canvas.addEventListener('mouseup', restoreCursor);
-
 window.addEventListener('resize', () => {
-  canvasHeight =
-    document.documentElement.clientHeight -
+    canvas.height =
+    document.documentElement.offsetHeight -
     form.offsetHeight -
     dateBar.offsetHeight;
-  canvasWidth = canvas.offsetWidth;
-  drawTable();
+    canvas.width = canvas.offsetWidth;
+    dateBar.width = canvas.width;
+    priceBar.height = canvas.height;
+drawTable();
 }); 
 
 async function fetchStocks(stockname, from, to){
@@ -68,26 +61,20 @@ async function requestData(e){
     drawTable();
 }
 
-function drawTable(){
+function drawTable() {
     const ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.strokeStyle = 'black';
     ctx.lineWidth = 1;
+    
     ctx.save();
-    console.log(canvasHeight);
-    canvas.width = canvasWidth;
-    canvas.height = canvasHeight;
-    ctx.translate(0 + canvasDragging.slidedPositionX, canvasHeight + canvasDragging.slidedPositionY);
+    ctx.translate(0 + canvasDragging.slidedPositionX, canvas.height + canvasDragging.slidedPositionY);
     let startIndex = viewRange.startIndex;
     let endIndex = viewRange.endIndex;
-    let sidebarWidth = canvasWidth / 5;
-    let bottombarHeight = canvasHeight / 5;
-    let availableWidthForCandles = canvasWidth - sidebarWidth;
-    let availableHeightForCandles = canvasHeight - bottombarHeight;
-    let bottombarWidth = canvasWidth - sidebarWidth;
 
     // Bar size and spacing initialization
     let range = endIndex - startIndex;
-    let availableSpacePerBar = canvasWidth / range;
+    let availableSpacePerBar = canvas.width / range;
     let barSpace = availableSpacePerBar / 3;
     let barWidth = availableSpacePerBar - barSpace;   
 
@@ -101,20 +88,17 @@ function drawTable(){
     range = endIndex - startIndex;
     let pivot = startIndex;
     // ratio: amount of huf per pixel Y axis
-    const ratio = (chartMaxValue - chartMinValue) / canvasHeight;
+    const ratio = (chartMaxValue - chartMinValue) / canvas.height;
     let barTopY;
     let barHeight;
 
-    let date;
-    let dateDrawStart = 10;
-    let dateDrawEnd = -10000;
-    let lastDate;
-    let dateSpace = bottombarWidth / 8;
+    clearDate();
+
     for (let i = 0; i < range; i++) {
         let closePrice = currentDatas[pivot].closePrice;
         let openPrice = currentDatas[pivot].openPrice;
         let barTopX = - (startIndexModifier * availableSpacePerBar) + i * barWidth + barSpace * i + barSpace / 4;
-        ctx.restore();
+        
         if (closePrice < openPrice){
             barTopY = -((openPrice - chartMinValue) / ratio);
             barHeight = (openPrice - closePrice) / ratio;
@@ -126,10 +110,10 @@ function drawTable(){
             ctx.strokeStyle = 'green';
             barHeight = (closePrice - openPrice) / ratio;
         }
-        ctx.save();
+        
         // draw bar
         ctx.fillRect(barTopX, barTopY, barWidth, barHeight);
-                  
+        
         // draw line from bar to maxprice
         ctx.beginPath();
         let barMiddlePointX = (barTopX+(barTopX+barWidth)) / 2;
@@ -146,60 +130,57 @@ function drawTable(){
         ctx.moveTo(barMiddlePointX, barTopY + barHeight);
         ctx.lineTo(barMiddlePointX, minPricePointY);
         ctx.stroke();
-        
-        
 
-        
-        
+        drawDate(barMiddlePointX, pivot);
+
         pivot++;
     }
+    ctx.restore();
 }
 
-function drawDates() {
+function drawDate(barMiddlePointX, pivot) {
     const ctxBottom = dateBar.getContext('2d');
+    ctxBottom.save();
+    ctxBottom.translate(0 + canvasDragging.slidedPositionX, 0);
     ctxBottom.fillStyle = 'black';
 
     let displayDate;
-    if (barTopX > dateDrawEnd) {
-        let currentDate = new Date(currentDatas[pivot].tradeDate);
+    let currentDate = new Date(currentDatas[pivot].tradeDate);
+    displayDate = currentDate.getDate();
 
-        if (lastDate == null || lastDate.getMonth() != currentDate.getMonth()) {
-            displayDate = month(currentDate.getMonth());
-        } else {
-            displayDate = currentDate.getDate();
-        }
-        console.log(currentDatas[pivot]);
+    ctxBottom.font = '20px Arial';
+    const textWidth = ctxBottom.measureText(displayDate).width;
+    ctxBottom.fillText(displayDate, barMiddlePointX - textWidth / 2, 40);
+    ctxBottom.restore();
+}
 
-        lastDate = currentDate;
-
-        //ctx.font = "20px Georgia";
-        ctxBottom.fillText(displayDate, barTopX, -10);
-        dateDrawEnd = barTopX + dateSpace + 10;
-    }
+function clearDate() {
+    const ctxBottom = dateBar.getContext('2d');
+    ctxBottom.fillStyle = 'grey';
+    ctxBottom.fillRect(0, 0, dateBar.width, dateBar.height);
+    
 }
 
 
 function dynamicStartIndex(startIndex, startIndexModifier) {
     startIndex -= startIndexModifier;
-    startIndex = Math.max(0, startIndex);
-    return startIndex;
+    return Math.max(0, startIndex);
+    
 }
 
 function calculateStartIndexVisibility(startIndex, availableSpacePerBar) {
     let startIndexModifier = Math.floor(canvasDragging.slidedPositionX / availableSpacePerBar);
-    startIndexModifier = Math.min(startIndexModifier, startIndex);
-    return startIndexModifier;
+    return Math.min(startIndexModifier, startIndex);
 }
 
 function calculateEndIndexVisibility(range, availableSpacePerBar) {
-    let endIndexModifier = Math.floor((canvasWidth - range * availableSpacePerBar) / availableSpacePerBar);
+    let endIndexModifier = Math.floor((canvas.width - range * availableSpacePerBar) / availableSpacePerBar);
     return endIndexModifier;
 }
 
 function dynamicEndIndex(endIndex, endIndexModifier) {
     endIndex += endIndexModifier;
-    endIndex = Math.min(endIndex, currentDatas.length);
-    return endIndex;
+    return Math.min(endIndex, currentDatas.length);
 }
 
 function calculateMin() {
@@ -237,9 +218,9 @@ function zoom(e){
 }
 
 function zoomOnData() {
-    if (mousePosition.x < canvasWidth / 3) {
+    if (mousePosition.x < canvas.width / 3) {
         viewRange.endIndex--;
-    } else if (mousePosition.x > (canvasWidth / 3) * 2) {
+    } else if (mousePosition.x > (canvas.width / 3) * 2) {
         viewRange.startIndex++;
     } else {
         viewRange.endIndex--;
@@ -256,15 +237,15 @@ function zoomOnData() {
 function zoomOutOfData() {
     let max = currentDatas.length;
     let min = 0;
-    if (mousePosition.x < canvasWidth / 3) {
-        viewRange.startIndex--;
-        if (viewRange.startIndex <= 0) viewRange.endIndex++;
-    } else if (mousePosition.x > (canvasWidth / 3) * 2) {
-        viewRange.endIndex++;
-        if (viewRange.endIndex >= max) viewRange.startIndex--;
+    if (mousePosition.x < canvas.width / 3) {
+      viewRange.startIndex--;
+      if (viewRange.startIndex <= 0) viewRange.endIndex++;
+    } else if (mousePosition.x > (canvas.width / 3) * 2) {
+      viewRange.endIndex++;
+      if (viewRange.endIndex >= max) viewRange.startIndex--;
     } else {
-        viewRange.endIndex++;
-        viewRange.startIndex--;
+      viewRange.endIndex++;
+      viewRange.startIndex--;
     }
     if (viewRange.startIndex <= min) {
         viewRange.startIndex = min;
@@ -295,7 +276,6 @@ function restoreCursor() {
 
 async function test(){
     currentDatas = await fetchStocks('richter', '2021-01-10', '2021-08-15');
-    console.log(currentDatas);
     viewRange.endIndex = currentDatas.length;
     chartMinValue = calculateMin();
     chartMaxValue = calculateMax();
@@ -307,30 +287,20 @@ test();
 
 
 function month(date) {
-  switch (date) {
-    case 0:
-      return 'Jan';
-    case 1:
-      return 'Feb';
-    case 2:
-      return 'Mar';
-    case 3:
-      return 'Apr';
-    case 4:
-      return 'May';
-    case 5:
-      return 'Jun';
-    case 6:
-      return 'Jul';
-    case 7:
-      return 'Aug';
-    case 8:
-      return 'Sept';
-    case 9:
-      return 'Okt';
-    case 10:
-      return 'Nov';
-    case 11:
-      return 'Dec';
-  }
+const months = [
+  'Jan',
+  'Feb',
+  'Mar',
+  'Apr',
+  'May',
+  'Jun',
+  'Jul',
+  'Aug',
+  'Sept',
+  'Oct',
+  'Nov',
+  'Dec',
+    ];
+    
+    return months[date] || '';
 }
